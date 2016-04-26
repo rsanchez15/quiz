5a052b8d47488ff6885cf865fda3465346f2a03a
rsanchez15 <r.sanchez-mateos@alumnos.upm.es>
2016-04-27 00:57:17 +0200

    Estilos css

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,7 +1,11 @@
 exports.question = function(req, res,next){
-	res.render('quizes/question', {question: 'Capital de Italia'});
+	var answer = req.query.answer || '';
+	res.render('quizes/question', {	question: 'Capital de Italia',
+									answer : answer});
 };
 exports.check = function(req,res,next){
+	var answer = req.query.answer || '';
 	var result = req.query.answer === 'Roma' ? 'Correcta' : 'Incorrecta';
-	res.render('quizes/result', {result: result});
+	res.render('quizes/result', {	result: result,
+									answer: answer});
 }
