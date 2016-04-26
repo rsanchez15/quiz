94d26b3ae9cbfcf1d61f4bad2880f06d5a310123
rsanchez15 <r.sanchez-mateos@alumnos.upm.es>
2016-04-26 13:23:11 +0200

    Primera pregunta

---
+++ controllers/quiz_controller.js
@@ -0,0 +1,7 @@
+exports.question = function(req, res,next){
+	res.render('quizes/question', {question: 'Capital de Italia'});
+};
+exports.check = function(req,res,next){
+	var result = req.query.answer === 'Roma' ? 'Correcta' : 'Incorrecta';
+	res.render('quizes/result', {result: result});
+}
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+var quizController =  require('../controllers/quiz_controller');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Quiz' });
@@ -9,5 +11,8 @@ router.get('/', function(req, res, next) {
 router.get('/author', function(req, res, next) {
   res.render('author', { title: 'Quiz' });
 });
+
+router.get('/question', quizController.question);
+router.get('/check', quizController.check);
 
 module.exports = router;
